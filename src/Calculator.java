fbec2dd560964a9ba970a999039877df625d058b
psns <psns0122@naver.com>
2025-01-08 16:33:30 +0900

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    // 1+1 = ?
+    public static void main(String[] args) {
+        // 1. 데이터 입력
+        Scanner in = new Scanner(System.in);
+        System.out.print("첫번째 숫자 : ");
+        int a = in.nextInt();
+        System.out.print("두번째 숫자 : ");
+        int b = in.nextInt();
+
+        int sum = 0;
+
+        // 2. 데이터 처리
+        // 3. 데이터 결과 저장
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+
+        // 4. 결과 출력
+        System.out.println(sum);
+
+    }
+}
---
+++ src/Ex1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        System.out.println("첫 번째 프로그램입니다.");
+        System.out.println("Fun Programming!");
+        System.out.println("두 번째 프로그램입니다. 한 줄에 여러 문장을 출력합니다.");
+        System.out.println("Programming! It's fun.");
+        System.out.println("""
+                세 번째 프로그램입니다.
+                줄을 바꾸어 출력합니다.
+                여기까지 출력하고
+                줄을 바꿉니다.""");
+        System.out.println("My name is Hong Gil Dong.\nI an 13 years old.");
+        System.out.printf("서식 문자로 출력하는 방법입니다.\n%s", "이것은 문자열을 나타냅니다.");
+    }
+}
